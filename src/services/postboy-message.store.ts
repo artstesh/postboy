d96d6039a56be8838f8af6e8cfc60bb4ea3c0e5f
artstesh <artstesh@mail.ru>
2026-01-15 09:34:51 +0300

    Warn on overriding existing messages and executors in postboy-message.store
    Added console warnings when registering a message or executor with an already existing ID, alerting developers about potential overrides.

--- src/services/postboy-message.store.ts
+++ src/services/postboy-message.store.ts
@@ -12,10 +12,14 @@ export class PostboyMessageStore {
   protected callbacks = new Map<string, (() => void)[]>();
 
   public registerMessage(id: string, sub: PostboySubscription<any>): void {
+    // tslint:disable-next-line:no-console
+    if (this.messages.has(id)) console.warn(`Message with id ${id} already registered. Overriding...`);
     this.messages.set(id, sub);
   }
 
   public registerExecutor(id: string, executor: (e: PostboyExecutor<any>) => any): void {
+    // tslint:disable-next-line:no-console
+    if (this.executors.has(id)) console.warn(`Executor with id ${id} already registered. Overriding...`);
     this.executors.set(id, executor);
   }
 

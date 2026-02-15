6a37c41deac3851872ebe6378f0d3bf0b3ec4d00
artstesh <artstesh@mail.ru>
2026-02-15 22:22:08 +0300

    Make `TestExecutor` and related handlers generic, updating type declarations and integration tests accordingly

--- src/models/postboy-executor.ts
+++ src/models/postboy-executor.ts
@@ -1,3 +1,5 @@
-import { PostboyMessage } from './postboy.message';
+import {PostboyMessage} from './postboy.message';
 
-export abstract class PostboyExecutor<T> extends PostboyMessage {}
+export abstract class PostboyExecutor<T> extends PostboyMessage {
+  declare protected readonly _postboyResultType?: T;
+}

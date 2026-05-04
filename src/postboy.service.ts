07a94e4269679c9491461ab8f375ddaf156b0f07
artstesh <artstesh@mail.ru>
2026-05-04 19:15:07 +0300

    3.3.1

--- src/postboy.service.ts
+++ src/postboy.service.ts
@@ -1,4 +1,4 @@
-import {first, Observable, Subject, tap} from 'rxjs';
+import { first, Observable, Subject, tap } from 'rxjs';
 import { checkId, PostboyGenericMessage } from './models/postboy-generic-message';
 import { PostboySubscription } from './models/postboy-subscription';
 import { PostboyExecutor } from './models/postboy-executor';
@@ -152,8 +152,7 @@ export class PostboyService {
 
       return () => subscription.unsubscribe();
     });
-    if (!!action)
-      observable.subscribe();
+    if (!!action) observable.subscribe();
     return observable;
   }
 

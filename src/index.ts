604603e335704923f5e3275440d3e7e5ad7bc0c3
artstesh <artstesh@mail.ru>
2026-05-23 16:33:49 +0300

    3.4.1

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ export * from './i-postboy-depending.service';
 export * from './postboy-abstract.registrator';
 export * from './services/postboy-middleware.service';
 export * from './services/postboy-message.store';
-export {PostboyNamespaceStore} from './services/postboy-namespace.store';
+export { PostboyNamespaceStore } from './services/postboy-namespace.store';
 export * from './messages/index';
 export * from './models/index';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,7 @@
-export {PostboyMessage} from './postboy.message';
-export {PostboyCallbackMessage} from './postboy-callback.message';
-export {PostboyExecutionHandler} from './postboy-execution.handler';
-export {PostboyExecutor} from './postboy-executor';
-export {PostboyGenericMessage, checkId} from './postboy-generic-message';
-export {PostboyMiddleware} from './postboy-middleware';
-export {PostboySubscription} from './postboy-subscription';
+export { PostboyMessage } from './postboy.message';
+export { PostboyCallbackMessage } from './postboy-callback.message';
+export { PostboyExecutionHandler } from './postboy-execution.handler';
+export { PostboyExecutor } from './postboy-executor';
+export { PostboyGenericMessage, checkId } from './postboy-generic-message';
+export { PostboyMiddleware } from './postboy-middleware';
+export { PostboySubscription } from './postboy-subscription';

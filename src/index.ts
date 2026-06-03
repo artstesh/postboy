c831d80c51e33ff155c9066eea397113a00e53f3
artstesh <artstesh@mail.ru>
2026-06-03 22:03:44 +0300

    Refactor: adjust TypeScript configs and refine exports for cleaner structure

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@
  * Public API Surface of postboy
  */
 
-export * from './postboy.service';
-export * from './i-postboy-depending.service';
-export * from './postboy-abstract.registrator';
-export * from './services/postboy-middleware.service';
-export * from './services/postboy-message.store';
-export { PostboyNamespaceStore } from './services/postboy-namespace.store';
+export {PostboyService} from './postboy.service';
+export {IPostboyDependingService} from './i-postboy-depending.service';
+export {PostboyAbstractRegistrator} from './postboy-abstract.registrator';
+export {type MessageType} from './postboy-abstract.registrator';
+export {PostboyMiddlewareService} from './services/postboy-middleware.service';
+export {PostboyMessageStore} from './services/postboy-message.store';
+export {PostboyNamespaceStore} from './services/postboy-namespace.store';
 export * from './messages/index';
 export * from './models/index';

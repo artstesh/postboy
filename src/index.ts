183e7cca634dc609e33a926edefeae6e6aa8ba14
artstesh <artstesh@mail.ru>
2026-06-03 22:06:14 +0300

    1.4.4

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
  * Public API Surface of postboy
  */
 
-export {PostboyService} from './postboy.service';
-export {IPostboyDependingService} from './i-postboy-depending.service';
-export {PostboyAbstractRegistrator} from './postboy-abstract.registrator';
-export {type MessageType} from './postboy-abstract.registrator';
-export {PostboyMiddlewareService} from './services/postboy-middleware.service';
-export {PostboyMessageStore} from './services/postboy-message.store';
-export {PostboyNamespaceStore} from './services/postboy-namespace.store';
+export { PostboyService } from './postboy.service';
+export { IPostboyDependingService } from './i-postboy-depending.service';
+export { PostboyAbstractRegistrator } from './postboy-abstract.registrator';
+export { type MessageType } from './postboy-abstract.registrator';
+export { PostboyMiddlewareService } from './services/postboy-middleware.service';
+export { PostboyMessageStore } from './services/postboy-message.store';
+export { PostboyNamespaceStore } from './services/postboy-namespace.store';
 export * from './messages/index';
 export * from './models/index';
